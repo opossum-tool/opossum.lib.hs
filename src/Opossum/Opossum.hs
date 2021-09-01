e27d441ea163116ec475ed1e806c737cd6fd2187
Maximilian Huber <maximilian.huber@tngtech.com>
2021-09-01 06:13:12 +0200

    fix build
    Signed-off-by: Maximilian Huber <maximilian.huber@tngtech.com>

---
+++ src/Opossum/Opossum.hs
@@ -0,0 +1,281 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE StrictData #-}
+module Opossum.Opossum
+  ( Opossum_Metadata (..)
+  , Opossum_Resources (..), countFiles
+  , fpToResources, fpsToResources
+  , Opossum_FrequentLicense (..)
+  , Opossum_Coordinates (..)
+  , Opossum_ExternalAttribution (..), Opossum_ExternalAttribution_Source (..)
+  , Opossum (..)
+  , writeOpossumStats
+  ) where
+
+import GHC.Generics
+import Data.UUID (UUID)
+import System.Random (randomIO)
+import System.IO (Handle, hPutStrLn, hClose, stdout)
+import qualified System.IO as IO
+import qualified Data.List as List
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import qualified Data.Aeson.Encode.Pretty as A
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as T
+import qualified Data.Maybe as Maybe
+import qualified Data.HashMap.Strict as HM
+import qualified System.Process as P
+import qualified Data.ByteString.Lazy as B
+import qualified System.FilePath as FP
+
+objectNoNulls :: [A.Pair] -> A.Value
+objectNoNulls = let
+  dropNulls []              = []
+  dropNulls ((_,A.Null):ps) = dropNulls ps
+  dropNulls (p:ps)          = p : (dropNulls ps)
+  in A.object . dropNulls
+
+data Opossum_Metadata
+  = Opossum_Metadata
+  { projectId :: String
+  , fileCreationDate :: String
+  } deriving (Show, Generic)
+instance A.ToJSON Opossum_Metadata where
+  toJSON (Opossum_Metadata pid fcd) = A.object
+    [ "projectId" A..= (T.pack pid)
+    , "fileCreationDate" A..= (T.pack fcd)
+    ]
+instance A.FromJSON Opossum_Metadata where
+  parseJSON = A.withObject "Opossum_FrequentLicense" $ \v -> do
+    Opossum_Metadata <$> v A..: "projectId"
+                 <*> v A..: "fileCreationDate"
+
+data Opossum_Resources
+  = Opossum_Resources 
+  { _dirs :: Map.Map FilePath Opossum_Resources
+  , _files :: Set.Set FilePath
+  } deriving (Show, Generic, Eq)
+instance A.ToJSON Opossum_Resources where
+    toJSON (Opossum_Resources dirs files) = let
+        pairsFromDirs = 
+            map (\(fragment, resources) -> ((T.pack fragment) A..= (A.toJSON resources)))
+            (Map.toList dirs) 
+        pairsFromFiles = map (\fragment ->  (T.pack fragment) A..= (1::Int)) $ Set.toList files
+      in A.object (pairsFromFiles ++ pairsFromDirs)
+instance A.FromJSON Opossum_Resources where
+  parseJSON = A.withObject "Opossum_Resources" $ \v -> let
+      isObject :: A.Value -> Bool
+      isObject (A.Object _) = True
+      isObject _            = False
+      getFiles :: [(FilePath, A.Value)] -> Set.Set FilePath
+      getFiles = Set.fromList . map (\(fp,_) -> fp) . (filter (\(_,v') -> not (isObject v')))
+      getDirs :: [(FilePath, A.Value)] -> A.Parser (Map.Map FilePath Opossum_Resources)
+      getDirs list = do
+        parsedList <- mapM (\(fp,o) -> do 
+          rs <- A.parseJSON o :: A.Parser Opossum_Resources
+          return (fp,rs)) (filter (\(_,v') -> isObject v') list)
+        return (Map.fromList parsedList)
+    in do
+      let vList = map (\(t,v') -> (T.unpack t, v')) $ HM.toList v
+      dirs <- getDirs vList
+      return (Opossum_Resources dirs (getFiles vList))
+instance Semigroup Opossum_Resources where
+  (Opossum_Resources dirs1 files1) <> (Opossum_Resources dirs2 files2) = let
+    dirs = (Map.unionWith (<>) dirs1 dirs2)
+    dirNames = Map.keys dirs
+    files = Set.filter (\f -> not $ f `elem` dirNames) $ files1 <> files2
+    in Opossum_Resources dirs files
+instance Monoid Opossum_Resources where
+  mempty = Opossum_Resources Map.empty Set.empty
+fpToResources :: Bool -> FilePath -> Opossum_Resources
+fpToResources isFile = let
+    fpToResources' :: [FilePath] -> Opossum_Resources
+    fpToResources' (f : [])  = if isFile
+                               then Opossum_Resources (Map.empty) (Set.singleton f)
+                               else Opossum_Resources (Map.singleton f mempty) Set.empty
+    fpToResources' ("/": fs) = fpToResources' fs
+    fpToResources' (f : fs)  = Opossum_Resources (Map.singleton f (fpToResources' fs)) Set.empty
+  in fpToResources' . (map FP.dropTrailingPathSeparator) . FP.splitPath
+fpsToResources :: [FilePath] -> Opossum_Resources
+fpsToResources = mconcat . map (fpToResources True)
+countFiles :: Opossum_Resources -> Int
+countFiles (Opossum_Resources dirs files) = length files + ((sum . map countFiles . Map.elems) dirs)
+
+data Opossum_ExternalAttribution_Source
+  = Opossum_ExternalAttribution_Source String Double
+  deriving (Show, Generic, Eq)
+instance A.ToJSON Opossum_ExternalAttribution_Source where
+    toJSON (Opossum_ExternalAttribution_Source source documentConfidence) =
+        A.object [ "name" A..= (T.pack source)
+                 , "documentConfidence" A..= documentConfidence
+                 ]
+instance A.FromJSON Opossum_ExternalAttribution_Source where
+    parseJSON = A.withObject "Opossum_ExternalAttribution_Source" $ \v -> do
+        Opossum_ExternalAttribution_Source <$> v A..: "name"
+                                       <*> v A..: "documentConfidence"
+
+data Opossum_Coordinates
+  = Opossum_Coordinates
+  { _packageName :: Maybe T.Text
+  , _packageNamespace :: Maybe T.Text
+  , _packageType :: Maybe T.Text
+  , _packageVersion :: Maybe T.Text
+  } deriving (Show, Generic, Eq)
+opoossumCoordinatesPreObjectList :: Opossum_Coordinates -> [A.Pair]
+opoossumCoordinatesPreObjectList 
+    (Opossum_Coordinates 
+        packageType
+        packageNamespace
+        packageName
+        packageVersion) = [ "packageType" A..= packageType
+                          , "packageNamespace" A..= packageNamespace
+                          , "packageName" A..= packageName
+                          , "packageVersion" A..= packageVersion
+                          ]
+instance A.ToJSON Opossum_Coordinates where
+    toJSON = objectNoNulls . opoossumCoordinatesPreObjectList
+instance A.FromJSON Opossum_Coordinates where
+    parseJSON = A.withObject "Opossum_Coordinates" $ \v -> do
+        packageType <- v A..:? "packageType"
+        packageNamespace <- v A..:? "packageNamespace"
+        packageName <- v A..:? "packageName"
+        packageVersion <- v A..:? "packageVersion"
+        return $ Opossum_Coordinates packageType packageNamespace packageName packageVersion
+
+data Opossum_ExternalAttribution
+  = Opossum_ExternalAttribution
+  { _source :: Opossum_ExternalAttribution_Source
+  , _attributionConfidence :: Double
+  , _comment :: Maybe T.Text
+  , _originId :: Maybe UUID
+  , _coordinates :: Opossum_Coordinates
+  , _copyright :: Maybe T.Text
+  , _licenseName :: Maybe T.Text
+  , _licenseText :: Maybe T.Text
+  , _preselected :: Bool
+  } deriving (Show, Generic, Eq)
+instance A.ToJSON Opossum_ExternalAttribution where
+    toJSON (Opossum_ExternalAttribution 
+        source
+        attributionConfidence
+        comment
+        originId
+        coordinates
+        copyright
+        licenseName
+        licenseText
+        preselected) = let
+            maybePreselected = if preselected
+              then Just True
+              else Nothing
+          in objectNoNulls ([ "source" A..= source
+                            , "attributionConfidence" A..= attributionConfidence
+                            , "comment" A..= comment
+                            , "copyright" A..= copyright
+                            , "licenseName" A..= licenseName
+                            , "licenseText" A..= licenseText 
+                            , "originId" A..= originId
+                            , "preSelected" A..= maybePreselected
+                            ] ++ (opoossumCoordinatesPreObjectList coordinates))
+instance A.FromJSON Opossum_ExternalAttribution where
+  parseJSON = A.withObject "Opossum_ExternalAttribution" $ \v -> do
+    source <- v A..: "source"
+    attributionConfidence <- fmap (100 `Maybe.fromMaybe`) (v A..:? "attributionConfidence")
+    comment <- v A..:? "comment"
+    originId <- fmap (fmap read . (\case
+      Just "" -> Nothing
+      x       -> x)) (v A..:? "originId" :: A.Parser (Maybe String))
+    coordinates <- A.parseJSON (A.Object v) :: A.Parser Opossum_Coordinates
+    copyright <- v A..:? "copyright"
+    licenseName <- fmap (\case
+      Just "" -> Nothing
+      l -> l) (v A..:? "licenseName")
+    licenseText <- fmap (\case
+      Just "" -> Nothing
+      l -> l) (v A..:? "licenseText")
+    preselected <- fmap (False `Maybe.fromMaybe`) (v A..:? "preSelected")
+
+    return (Opossum_ExternalAttribution source attributionConfidence comment originId coordinates copyright licenseName licenseText preselected)
+
+data Opossum_FrequentLicense
+  = Opossum_FrequentLicense
+  { shortName :: T.Text
+  , fullName :: T.Text
+  , defaultText :: T.Text
+  } deriving (Eq, Show, Generic)
+instance A.ToJSON Opossum_FrequentLicense where
+  toJSON (Opossum_FrequentLicense sn fn dt) = A.object [ "shortName" A..= sn
+                                                   , "fullName" A..= fn
+                                                   , "defaultText"  A..= dt
+                                                   ]
+instance A.FromJSON Opossum_FrequentLicense where
+  parseJSON = A.withObject "Opossum_FrequentLicense" $ \v -> do
+    Opossum_FrequentLicense <$> v A..: "shortName"
+                        <*> v A..: "fullName"
+                        <*> v A..: "defaultText"
+
+data Opossum
+  = Opossum
+  { _metadata :: Maybe Opossum_Metadata
+  , _resources :: Opossum_Resources
+  , _externalAttributions :: Map.Map UUID Opossum_ExternalAttribution
+  , _resourcesToAttributions :: Map.Map FilePath [UUID]
+  , _frequentLicenses :: [Opossum_FrequentLicense]
+  } deriving (Show, Generic)
+instance A.ToJSON Opossum where
+    toJSON (Opossum
+        metadata
+        resources
+        externalAttributions
+        resourcesToAttributions
+        frequentLicenses) = objectNoNulls
+          [ "metadata" A..= metadata
+          , "resources" A..= resources
+          , "externalAttributions" A..= externalAttributions
+          , "resourcesToAttributions" A..= resourcesToAttributions
+          , "frequentLicenses" A..= frequentLicenses
+          ]
+instance A.FromJSON Opossum where
+  parseJSON = A.withObject "Opossum" $ \v -> do
+    let
+      resourcesParser = fmap (mempty `Maybe.fromMaybe`) $ v A..:? "resources"
+      externalAttributionsParser = fmap (mempty `Maybe.fromMaybe`) $ v A..:? "externalAttributions"
+      resourcesToAttributionsParser = fmap (mempty `Maybe.fromMaybe`) $ v A..:? "resourcesToAttributions"
+    Opossum <$> v A..:? "metadata"
+        <*> resourcesParser
+        <*> externalAttributionsParser
+        <*> resourcesToAttributionsParser
+        <*> (fmap (\case 
+            Just fls -> fls
+            Nothing -> []) (v A..:? "frequentLicenses"))
+instance Semigroup Opossum where
+    hhc1 <> hhc2 = let
+          mergedResources = _resources hhc1 <> _resources hhc2 
+          mergedExternalAttributions = Map.union (_externalAttributions hhc1) (_externalAttributions hhc2)
+          mergedResourcesToAttributions = Map.unionWith (++) (_resourcesToAttributions hhc1) (_resourcesToAttributions hhc2) -- TODO: nub
+          mergedFrequentLicenses = List.nub (_frequentLicenses hhc1 ++ _frequentLicenses hhc2)
+        in Opossum (_metadata hhc1) 
+               mergedResources
+               mergedExternalAttributions
+               mergedResourcesToAttributions
+               mergedFrequentLicenses
+instance Monoid Opossum where
+    mempty = Opossum Nothing mempty Map.empty Map.empty []
+
+writeOpossumStats :: Opossum -> IO ()
+writeOpossumStats (Opossum { _metadata = m
+                   , _resources = rs
+                   , _externalAttributions = eas
+                   , _resourcesToAttributions = rtas
+                   , _frequentLicenses = fls
+                   }) = do
+                     putStrLn ("metadata: " ++ show m)
+                     putStrLn ("resources: #files=" ++ (show (countFiles rs)))
+                     putStrLn ("externalAttributions: #=" ++ (show (length eas)))
+                     putStrLn ("resourcesToAttributions: #=" ++ (show (length rtas)))
+                     putStrLn ("frequentLicenses: #=" ++ (show (length fls)))
---
+++ src/Opossum/OpossumUtils.hs
@@ -0,0 +1,146 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE StrictData #-}
+module Opossum.OpossumUtils
+  ( clusterifyOpossum 
+  , unDot
+  , dropDir
+  , computeMergedOpossum
+  , mergifyEA
+  ) where
+
+import Opossum.Opossum
+
+import System.IO (Handle, hPutStrLn, hClose, stdout)
+import qualified System.IO as IO
+import qualified Data.List as List
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import qualified Data.Aeson.Encode.Pretty as A
+import qualified Data.Map as Map
+import           Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import qualified System.FilePath as FP
+import qualified Data.ByteString.Lazy as B
+import Data.UUID (UUID)
+import System.Random (randomIO)
+
+mergifyCopyright :: Maybe Text -> Maybe Text -> Maybe Text
+mergifyCopyright left Nothing = left
+mergifyCopyright Nothing right = right
+mergifyCopyright (Just c) (Just c') = Just . T.unlines . List.nub $ (T.lines c) ++ (T.lines c')
+
+cleanupLicense :: Maybe Text -> Maybe Text
+cleanupLicense Nothing  = Nothing
+cleanupLicense (Just "") = Nothing
+cleanupLicense (Just t) = case T.stripPrefix ", " t of
+    Nothing -> Just t
+    t'      -> t'
+
+matchesCoordinates :: Opossum_Coordinates -> Opossum_Coordinates -> Bool
+matchesCoordinates = undefined
+
+mergifyEA :: Opossum_ExternalAttribution -> Opossum_ExternalAttribution -> Maybe Opossum_ExternalAttribution
+mergifyEA
+  left@(Opossum_ExternalAttribution
+  { _source = Opossum_ExternalAttribution_Source source _
+  , _attributionConfidence = attributionConfidence
+  , _comment = comment
+  , _originId = originId
+  , _coordinates = coordinates
+  , _copyright = copyright
+  , _licenseName = licenseName
+  , _licenseText = licenseText
+  , _preselected = preselected
+  }) 
+  (Opossum_ExternalAttribution
+  { _source = Opossum_ExternalAttribution_Source source' _
+  , _attributionConfidence = attributionConfidence'
+  , _comment = comment'
+  , _originId = originId'
+  , _coordinates = coordinates'
+  , _copyright = copyright'
+  , _licenseName = licenseName'
+  , _licenseText = licenseText'
+  , _preselected = preselected'
+  }) 
+ = if (and [ source == source'
+           , coordinates `matchesCoordinates` coordinates'
+           , (cleanupLicense licenseName) == (cleanupLicense licenseName')])
+   then Just (left{ _attributionConfidence = (attributionConfidence `min` attributionConfidence')
+                  , _copyright = mergifyCopyright copyright copyright'
+                  , _licenseName = cleanupLicense licenseName
+                  , _licenseText = case licenseText of
+                    Just lt -> Just lt
+                    _       -> licenseText'
+                  , _preselected = preselected || preselected'
+                  })
+   else Nothing
+
+clusterifyEAMap :: Map.Map UUID Opossum_ExternalAttribution -> [(UUID, Opossum_ExternalAttribution, [UUID])]
+clusterifyEAMap = let
+        clusterifyEAMap' :: [(UUID, Opossum_ExternalAttribution)]
+                         -> [(UUID, Opossum_ExternalAttribution, [UUID])]
+                         -> [(UUID, Opossum_ExternalAttribution, [UUID])]
+        clusterifyEAMap' [] out               = out
+        clusterifyEAMap' (i:ins) out = let
+            clusterifyEAMap'' :: (UUID, Opossum_ExternalAttribution)
+                             -> [(UUID, Opossum_ExternalAttribution, [UUID])]
+                             -> [(UUID, Opossum_ExternalAttribution, [UUID])]
+            clusterifyEAMap'' (uuid, ea) []                              = [(uuid, ea, [])]
+            clusterifyEAMap'' (uuid, ea) (ins@(in'@(uuid', ea', uuids):ins')) 
+                | uuid == uuid' = ins
+                | otherwise     = case mergifyEA ea ea' of
+                    Just mergedEA -> (uuid', mergedEA, uuid : uuids):ins'
+                    Nothing -> in' : (clusterifyEAMap'' (uuid, ea) ins')
+            in clusterifyEAMap' ins (clusterifyEAMap'' i out)
+      
+    in (`clusterifyEAMap'` []) . Map.assocs
+
+clusterifyOpossum :: Opossum -> Opossum
+clusterifyOpossum (hhc@Opossum { _externalAttributions = eas , _resourcesToAttributions = rtas }) = let
+    clusters = clusterifyEAMap eas
+    newEas = (Map.fromList . map (\(uuid, ea, _) -> (uuid, ea))) clusters
+    lookupMap = ( Map.fromList . concatMap (\(uuid, _, uuids) -> map (\uuid' -> (uuid', uuid)) uuids)) clusters
+    newRtas = (Map.map (\uuids -> List.nub (map (\uuid -> Map.findWithDefault uuid uuid lookupMap) uuids))) rtas
+  in hhc { _externalAttributions = newEas
+         , _resourcesToAttributions = newRtas
+         }
+
+unDot :: Opossum -> Opossum
+unDot (hhc@Opossum { _resources = rs , _resourcesToAttributions = rtas }) = let
+  undotResources (rs@Opossum_Resources { _dirs = dirs }) = let
+    contentOfDot = Map.findWithDefault mempty "." dirs
+    dirsWithoutDot = Map.delete "." dirs
+    in rs {_dirs = dirsWithoutDot} <> contentOfDot
+  undotRTAS = Map.mapKeys (\k -> case List.stripPrefix "/." k of
+    Nothing -> k
+    Just k' -> k')
+  in hhc{ _resources = undotResources rs , _resourcesToAttributions = undotRTAS rtas }
+
+dropDir :: FilePath -> Opossum -> Opossum
+dropDir directoryName (hhc@Opossum{ _resources = rs, _resourcesToAttributions = rtas}) = let
+  filterResources (rs@Opossum_Resources { _dirs = dirs }) = let
+    filteredDirs = Map.filterWithKey (\key -> const (not (directoryName == key))) dirs
+    filteredAndCleanedDirs = Map.map filterResources filteredDirs
+    in rs{ _dirs = filteredAndCleanedDirs }
+  filterRTAS = Map.filterWithKey (\key -> const (not (directoryName `List.isSubsequenceOf` key))) 
+  in hhc{ _resources = filterResources rs, _resourcesToAttributions = filterRTAS rtas}
+
+computeMergedOpossum :: [FilePath] -> IO B.ByteString
+computeMergedOpossum inputPaths = let
+    parseOpossum :: FP.FilePath -> IO Opossum
+    parseOpossum fp = do
+      hPutStrLn IO.stderr ("parse: " ++ fp)
+      bs <- B.readFile fp
+      case A.eitherDecode' bs of
+        Right hhc -> return hhc
+        Left err  -> do
+            fail err
+  in do
+    hhcs <- mapM parseOpossum inputPaths
+    let finalOpossum = clusterifyOpossum $ mconcat (map unDot hhcs)
+    return (A.encodePretty finalOpossum)
